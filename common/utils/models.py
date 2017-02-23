e4a0af526ddacae5d9bbc678bff54ba8374da72b
utkarsh <utkarsh9891@gmail.com>
2017-02-23 19:27:59 +0530

    added models.py back for reference to MetaDataModel

---
+++ common/utils/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from common.utils.model_fields import DefaultTZDateTimeField
+
+
+class MetaDataModel(models.Model):
+    """
+    Base model to be inherited across the project
+    """
+    created_by = models.CharField(max_length=100, default='NA')
+    modified_by = models.CharField(max_length=100, default='NA')
+    created_at = DefaultTZDateTimeField(auto_now_add=True)
+    modified_at = DefaultTZDateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
